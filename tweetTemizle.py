8b42137afe8b4a84b387691112567634e8b281b5
tugbaficici <tugbaficici98@gmail.com>
2021-01-01 17:52:45 +0300

    Doğru haberler oluşturuldu

---
+++ tweetTemizle.py
@@ -0,0 +1,112 @@
+import csv
+import string
+import re
+
+tweets = list()
+truetweets = list()
+
+with open('Haberler/truetweets.csv') as truetweet:
+    tweets = truetweet.readlines()
+    keyword_list = ["corona", "coronavirus", "korona", "koronavirus", "covid-19", "kovid-19","sağlık bakanı"]
+
+    for i in tweets:
+        for j in keyword_list:
+            if j in i.lower():
+                truetweets.append(i)
+                break
+
+
+#Haberlerde bulunan linklerin temizlenmesi
+linksiztweet1=list()
+linksiztweet=list()
+for tweet in truetweets:
+    if "https://t.co/" in tweet:
+        baslangicIndex=tweet.index("https://t.co/")
+        sonIndex=baslangicIndex+23
+        cikarilcakLink=tweet[baslangicIndex:sonIndex]
+        tweet=tweet.replace(cikarilcakLink,"")
+        linksiztweet1.append(tweet)
+    else:
+        linksiztweet1.append(tweet)
+
+for tweet in linksiztweet1:
+    if "https://t.co/" in tweet:
+        baslangicIndex=tweet.index("https://t.co/")
+        sonIndex=baslangicIndex+23
+        cikarilcakLink=tweet[baslangicIndex:sonIndex]
+        tweet=tweet.replace(cikarilcakLink,"")
+        linksiztweet.append(tweet)
+    else:
+        linksiztweet.append(tweet)
+
+
+
+#Haberlerde bulunan kelimeler küçük harflere çevrilmesi
+#Noktalama işaretleri temizlenecek
+punctuation = ['(', ')', '?', ':', ';', ',', '.', '!', '/', '"', "'"]
+for i in range(len(linksiztweet)):
+    linksiztweet[i]=linksiztweet[i].lower()
+    yenitweet = ""
+    for j in linksiztweet[i]:
+        if j not in punctuation:
+            yenitweet += j
+    linksiztweet[i]=yenitweet
+
+
+
+#Stop words(etkisiz kelimeler) haberlerin içinden çıkartılacak
+turkçeStopWords=['acaba','ama','artık','asla','aslında','az','ancak',
+            'bana','bazen','bazı','bazıları', 'bazısı', 'belki', 'ben', 'beni','benim', 'beş',
+            'bile', 'bir', 'birçoğu', 'birçok', 'birçokları','biri', 'birisi', 'birkaç', 'birkaçı',
+            'birşey', 'birşeyi', 'biz','bize', 'bizi', 'bizim', 'böyle', 'böylece', 'bu', 'buna',
+            'bunda', 'bundan', 'bunu','bunun', 'burada', 'bütün',
+            'çoğu', 'çoğuna', 'çoğunu', 'çok', 'çünkü',
+            'da', 'daha', 'de', 'değil', 'demek', 'diğer', 'diğeri', 'diğerleri', 'diye', 'dolayı',
+            'elbette', 'en',
+            'fakat', 'falan', 'felan', 'filan',
+            'gene', 'gibi',
+            'hangi', 'hangisi', 'hani', 'hatta', 'hem', 'henüz', 'hep', 'hepsi', 'hepsine', 'hepsini',
+            'her', 'her biri', 'herkes', 'herkese', 'herkesi', 'hiç', 'hiç kimse', 'hiçbiri', 'hiçbirine', 'hiçbirini',
+            'için', 'içinde', 'ile', 'ise', 'işte',
+            'kaç', 'kadar', 'kendi', 'kendine', 'kendini', 'ki', 'kim', 'kime', 'kimi', 'kimin', 'kimisi',
+            'madem', 'mı', 'mi', 'mu', 'mü',
+            'nasıl', 'ne', 'ne kadar', 'ne zaman', 'neden', 'nedir', 'nerde', 'nerede', 'nereden', 'nereye', 'nesi', 'neyse', 'niçin', 'niye',
+            'ona', 'ondan', 'onlar', 'onlara', 'onlardan', 'onların', 'onu', 'onun', 'orada', 'oysa', 'oysaki',
+            'öbürü', 'ön', 'önce', 'ötürü', 'öyle',
+            'sana', 'sen', 'senden', 'seni', 'senin', 'siz', 'sizden', 'size', 'sizi', 'sizin', 'son', 'sonra',
+            'şayet', 'şey', 'şimdi', 'şöyle', 'şu', 'şuna', 'şunda', 'şundan', 'şunlar', 'şunu', 'şunun',
+            'tabi', 'tamam', 'tüm', 'tümü',
+            'üzere',
+            'var', 've', 'veya', 'veyahut',
+            'ya', 'ya da', 'yani', 'yerine', 'yine', 'yoksa',
+            'zaten', 'zira']
+
+for i in range(len(linksiztweet)):
+    words=linksiztweet[i].split(" ")
+    yenitweet = ""
+    for j in words:
+        if j not in turkçeStopWords:
+            yenitweet += j+" "
+    linksiztweet[i]=yenitweet
+
+#Tweetlerde bulunan emojilerin temizlenmesi
+regrex_pattern = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # Genel emoji seti
+        u"\U0001F300-\U0001F5FF"  # Semboller
+        u"\U0001F680-\U0001F6FF"  # Harita sembolleri
+        u"\U0001F1E0-\U0001F1FF"  # Bayrak sembolleri
+        u"\U0001F9A0" #Mikrop emojisi
+        u"\U0001F498" #Aşı emojisi
+        u"\U00002757" #Ünlem emojisi
+
+                           "]+", flags = re.UNICODE)
+
+for i in range(len(linksiztweet)):
+    linksiztweet[i]=regrex_pattern.sub(r'',linksiztweet[i])
+
+
+
+with open('Haberler/truetweetssecilen.csv','w') as temizveri:
+    for i in range(len(linksiztweet)):
+        temizveri.write(linksiztweet[i])
+    
